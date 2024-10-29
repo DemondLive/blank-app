47d6f3c84f056a18fdc560665e7db19da83a21fd
DemondLive <anaislamaslinda993@gmail.com>
2024-10-29 15:18:45 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,15 @@
+
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
+st.sidebar.title("my first barra lateral")
+st.sidebar.header("holis barra lateral")
+st.sidebar.write("esto es una barra lateral")
+
+st.sidebar.image("_93356478_trumpyobama1epanov10.jpg")
+
+if st.sidebar.button("haz clik in the barra lateral"):
+    st.sidebar.write("clik hecho")
+
+user_input = st.sidebar.text_input("esribe algo")
+st.sidebar.write(f"escribiste in the barra: ", user_input)
